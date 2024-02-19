39c5f58ff94739860d996076df894547556705e9
AlexNek <alexnek@russinger.com>
2024-02-19 13:13:14 +0100

    model refactoring

---
+++ BlazorSortableList.Lib/SortableListModel.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorSortableList;
+
+public class SortableListModel<T>: ISortableListModel<T>
+{
+    protected List<T> _items;
+
+    public SortableListModel(List<T> items)
+    {
+        _items = items;
+    }
+
+    public string Group { get; set; } = Guid.NewGuid().ToString();
+
+    public SortableListSettings Settings { get; set; } = new SortableListSettings();
+
+    public IList<T> Items
+    {
+        get
+        {
+            return _items;
+        }
+    }
+    
+}
