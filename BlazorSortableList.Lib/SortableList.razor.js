414ffc1aa35b7c242e363f4cd4e306a3668f2077
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-05-27 20:37:58 +0200

    fix: finally fixedddd

--- BlazorSortableList.Lib/SortableList.razor.js
+++ BlazorSortableList.Lib/SortableList.razor.js
@@ -19,32 +19,64 @@ export function init(id, group, pull, put, sort, handle, filter, component, forc
         forceFallback: forceFallback,
         handle: handle || undefined,
         onUpdate: (event) => {
+            let customChildNodes = Array.from(event.to.childNodes).filter(node => node.tagName === 'DIV');
             if (DEBUG_MODE) {
-                console.log("onUpdate:");
-                console.log(event);
+                console.log(event)
+                console.log("remove item for update:");
+                console.log(event.item);
+                console.log('event to: ', event.to)
+                console.log('event oldIndex: ', event.oldIndex)
+                console.log('event newIndex: ', event.newIndex)
+                console.log('event to childNodes: ', customChildNodes)
+                console.log('insert before: ', customChildNodes[event.oldIndex])
             }
+            event.item.remove();
 
+            if (event.oldIndex < event.newIndex)
+                event.to.insertBefore(event.item, customChildNodes[event.oldIndex])
+            else
+                event.to.insertBefore(event.item, customChildNodes[event.oldIndex+1])
+
+            // method inserts a child node before an existing child. insertBefore(newNode, referenceNode)
+            // referenceNode - The node before which newNode is inserted
+            //event.to.insertBefore(event.item, event.to.childNodes[event.oldIndex]);
             let oldIndex = event.oldDraggableIndex;
-            let newIndex = event.newDraggableIndex;
+            let newIndex = event.newDraggableIndex
 
             // Notify .NET to update its model and re-render
-            component.invokeMethodAsync('OnUpdateJS', oldIndex, newIndex, event.from.id);
+             component.invokeMethodAsync('OnUpdateJS', oldIndex, newIndex, event.from.id);
         },
         onRemove: (event) => {
+            let customChildNodes = Array.from(event.from.childNodes).filter(node => node.tagName === 'DIV');
             if (DEBUG_MODE) {
-                console.log("onRemove:");
-                console.log(event);
+                console.log(event)
+                console.log("remove item for update:");
+                console.log(event.item);
+                console.log('event to: ', event.to)
+                console.log('event oldIndex: ', event.oldIndex)
+                console.log('event newIndex: ', event.newIndex)
+                console.log('event from childNodes: ', customChildNodes)
+                console.log('insert before: ', customChildNodes[event.oldIndex])
             }
 
-            if (event.pullMode === 'clone') {
-                // Remove the clone
-                event.clone.remove();
-            }
             let oldIndex = event.oldDraggableIndex;
             let newIndex = event.newDraggableIndex;
+
+            // Revert the DOM to match the .NET state
+            event.item.remove();
+
+            event.from.insertBefore(event.item, customChildNodes[event.oldIndex]);
 
             // Notify .NET to update its model and re-render
             component.invokeMethodAsync('OnRemoveJS', oldIndex, newIndex, event.from.id, event.to.id);
+        },
+        onMove: (event) => {
+            // This event fires continually as you drag.
+            // You typically wouldn't do DOM manipulation here.
+            // It's useful for preventing drops, etc.
+            // If you return false, the item cannot be dropped at that position.
+            // return !event.related.classList.contains('no-drop');
+            return true; // Allow all moves by default
         }
     });
 }
